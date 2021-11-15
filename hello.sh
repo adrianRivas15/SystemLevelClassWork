0ff457448daf3a6a0dfa910bd97a8bf664519834
adrianRivas15 <77635532+adrianRivas15@users.noreply.github.com>
2021-11-15 17:17:54 -0500

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Welcome to computer science society"
+#Adrian Rivas
+#arivas7@student.gdu.edu
+echo "date is = `date`"
+echo */ | wc
+echo #PATH
+echo #USER
+echo #SHELL
+echo "Printing disk usage"
+echo "`df`"
+echo "Could you loan me "$" 25.00?"
+echo "if x = 2, x*x = 4, x/2 = 1"
+echo "`ls * .sh | grep c`"
+echo "Good Bye"
+echo "Current Hour = `date +"%H"`"
---
+++ myName.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+printf("My name is, Adrian Rivas\n");
+return 0;
+}
